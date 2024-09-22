9f3150e3ed9d93f32ac8d7b6b0ad7891bec3a504
Johirul-islam-6 <johirul-islam-574206@gmail.com>
2024-09-22 12:23:16 +0600

    Responsive device home container

--- src/Components/Home/HomeContainer.jsx
+++ src/Components/Home/HomeContainer.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./HomeContainer.css";
+import { FaEllipsisV } from "react-icons/fa";
 export const HomeContainer = () => {
   const [focusedIndex, setFocusedIndex] = useState(1);
 
@@ -17,13 +18,16 @@ export const HomeContainer = () => {
   ];
   return (
     <>
-      <div className=" ">
+      <div className="flex md:block justify-between h-[50px] md:h-[auto] items-center py-[3px] md:py-[0]">
         {/* -----------top Heading --------- */}
-        <h2 className="HomeContainer-Heading  border-l-[10px] border-[#46AEF7] ps-[15px]">
+        <h2 className="HomeContainer-Heading md:text-[24px] text-[16px]  border-l-[4px] md:border-l-[10px] border-[#46AEF7] ps-[15px]">
           フォローリスト
         </h2>
+        <h2 className="flex md:hidden text-[#46AEF7] ">
+          <FaEllipsisV />
+        </h2>
         {/* ------------ home container button ------ */}
-        <div className="flex gap-[13px] mt-[30px]">
+        <div className="hidden md:flex gap-[13px] mt-[30px]">
           {HomeContainerButton?.map((button, index) => (
             <button
               onClick={() => setFocusedIndex(index)}
@@ -41,7 +45,7 @@ export const HomeContainer = () => {
           ))}
         </div>
         {/* ----------- button total count item ------- */}
-        <div className="flex gap-[13px] mt-[6px]">
+        <div className="hidden md:flex gap-[13px] mt-[6px]">
           <button className="w-[159px] h-[33px] text-[#757575] text-[12px] font-[400] ">
             TOTAL{" "}
             <span className="text-[14px] text-[#46AEF7] font-[700]">100</span>
@@ -53,7 +57,7 @@ export const HomeContainer = () => {
         </div>
 
         {/* --------------- driven ----------- */}
-        <div className="mt-[10px] py-[19px] px-[80px] flex gap-[80px] border-t-[1px] border-b-[1px] border-[#757575]">
+        <div className="hidden md:flex mt-[10px] py-[19px] px-[80px]  gap-[80px] border-t-[1px] border-b-[1px] border-[#757575]">
           <h1 className="text-[16px] font-[500] text-[#757575]">
             ユーザーネーム
           </h1>
